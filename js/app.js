6a797a58db06796b773b77f2db5c6b1ee988dd2a
MontserratLili <lauraliligm@gmail.com>
2017-08-13 16:48:11 -0500

    terminandO portfolio

--- js/app.js
+++ js/app.js
@@ -2,6 +2,6 @@ $(document).ready(function () {
     setTimeout(function () {
         document.getElementById("bienvenido").style.display = "none";
         location.href = ("#home");
-    }, 3000);
+    }, 4000);
     $('.carousel.carousel-slider').carousel({fullWidth: true});
 });
